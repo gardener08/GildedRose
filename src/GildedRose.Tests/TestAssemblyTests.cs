98f5e42d8ad138d948cfb69037538785d4007ddd
ApplicationDeveloper <ApplicationDeveloper@ADAM236>
2018-10-03 15:01:47 -0500

    Added another test.  I identified an apparent bug in when we are incrementing SellIn.  We aren't incrementing it until the end of the day, which caused the new test to fail.  I will wait to fix this bug until more tests are written as I inherited this code.

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -128,5 +128,40 @@ namespace GildedRose.Tests
             Assert.Equal(itemUnderTest.Quality, 5);
 
         }
+
+        [Fact]
+        public void TestBackstagePassesOneDayBeforeConcert()
+        {
+            TestAssemblyTests programInstance = new TestAssemblyTests();
+            var programItems = programInstance.Items;
+            Item itemUnderTest = programItems[4];
+            Assert.Equal(itemUnderTest.SellIn, 15);
+            Assert.Equal(itemUnderTest.Quality, 20);
+
+            // TODO: Make this a loop (14 times)
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+
+            Assert.Equal(itemUnderTest.SellIn, 1);
+            // This test is failing because we are incrementing the SellIn member in the wrong spot of the code.
+            // We should be doing that at the start of the loop.
+            // We are currently relying on it both before and after it is incremented, and inconsistent results will occur.
+            // I will wait to fix the code until I have more unit tests completed, as I inherited this code
+            // The actual value is currently 47 because sellIn is being incremented at the end of the day.
+            // When we are on day 10, the code is treating it as day 11 for the purposes of Backstage pass value.
+            Assert.Equal(itemUnderTest.Quality, 49);
+        }
     }
 }
