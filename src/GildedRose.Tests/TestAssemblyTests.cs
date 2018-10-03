7ec4e164403559c36efa8d3cdfdebb242bacd76e
ApplicationDeveloper <ApplicationDeveloper@ADAM236>
2018-10-03 11:16:01 -0500

    Made Program class public to enable unit testing.  Added a reference to the main project into the test project.  Made items list protected internal.  I added protected to allow the unit tests to access this member.  I added internal because that is what it was before by default and I want to limit what I am doing in each refactoring step.
    Made the list and the UpdateQuality method static so that I don't run into conflicts between instance and non-instance methods.  The client allowed that but was silent on changing the main method.  The Program instance inside the main method isn't ever in a scope such that I can access it from a unit test.  That program instance was used to run the UpdateQuality instance method.

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,13 +1,21 @@
+using System.Collections.Generic;
 using Xunit;
+using GildedRose.Console;
 
 namespace GildedRose.Tests
 {
-    public class TestAssemblyTests
+    public class TestAssemblyTests : Program
     {
         [Fact]
         public void TestTheTruth()
         {
             Assert.True(true);
+        }
+
+        [Fact]
+        public void TestSetupItems()
+        {
+            Assert.NotNull(Program.Items);
         }
     }
 }
