a867590a3ecfcfc77ddf3f310bd6ee174d8a055d
ApplicationDeveloper <ApplicationDeveloper@ADAM236>
2018-10-03 14:08:45 -0500

    Added three more unit tests.  One is failing due to an ambiguous requirement.  Checking in the failing test for now and moving on until I get clarity on what needs to happen.  See comment in test method for details.

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -53,5 +53,80 @@ namespace GildedRose.Tests
             Assert.Equal(standardItem.SellIn, 4);
             Assert.Equal(standardItem.Quality, 6);
         }
+
+        [Fact]
+        public void TestForNoNegativeQuality()
+        {
+            TestAssemblyTests programInstance = new TestAssemblyTests();
+            var programItems = programInstance.Items;
+            Item itemUnderTest = programItems[2];
+            Assert.Equal(itemUnderTest.SellIn, 5);
+            Assert.Equal(itemUnderTest.Quality, 7);
+
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality(); // This shouldn't cause a quality of -1
+
+            Assert.Equal(itemUnderTest.SellIn, -3); // Code instead of comments
+            Assert.Equal(itemUnderTest.Quality, 0);
+        }
+
+        [Fact]
+        public void TestForDoubleSpeedQualityDegradationAfterSellByDate()
+        {
+            TestAssemblyTests programInstance = new TestAssemblyTests();
+            var programItems = programInstance.Items;
+            Item itemUnderTest = programItems[0];
+            Assert.Equal(itemUnderTest.SellIn, 10);
+            Assert.Equal(itemUnderTest.Quality, 20);
+
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality(); // Sell By date reached
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+
+            Assert.Equal(itemUnderTest.SellIn, -3);
+            Assert.Equal(itemUnderTest.Quality, 4);
+        }
+
+        [Fact]
+        public void TestIncreasingQualityOfAgedBrie()
+        {
+            TestAssemblyTests programInstance = new TestAssemblyTests();
+            var programItems = programInstance.Items;
+            Item itemUnderTest = programItems[1];
+            Assert.Equal(itemUnderTest.SellIn, 2);
+            Assert.Equal(itemUnderTest.Quality, 0);
+
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+            programInstance.UpdateQuality();
+
+            Assert.Equal(itemUnderTest.SellIn, -3);
+            // The following is failing.  The acceptance criteria specifies that "Aged Brie" actually increases in Quality the older it gets.
+            // I assumed that means by one quality point.  It is increasing by two after passing its expiration date.
+            // Because I can't find this behavior specified in the story, I will need to have a conversation with the Product Owner to verify
+            // that this is intended behavior and then we can either put in a fix or update the story.
+
+            // I will forgo the general rule of making this test pass before proceeding as this is a roadblock that I can navigate around for now.
+            Assert.Equal(itemUnderTest.Quality, 5);
+
+        }
     }
 }
