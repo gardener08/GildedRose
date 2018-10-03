996f1e412161bf06e9b36457d777dd760e88f87c
ApplicationDeveloper <ApplicationDeveloper@ADAM236>
2018-10-03 11:53:25 -0500

    Added test for sulfuras legendary item.

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using Xunit;
 using GildedRose.Console;
 
@@ -16,6 +17,24 @@ namespace GildedRose.Tests
         public void TestSetupItems()
         {
             Assert.NotNull(Program.Items);
+        }
+
+        [Fact]
+        public void TestLegendaryItem()
+        {
+            var programItems = Program.Items;
+            Item legendaryItem = programItems[3];
+            Assert.Contains("Sulfuras",legendaryItem.Name);
+            int currentSellIn = legendaryItem.SellIn;
+            int currentQuality = legendaryItem.Quality;
+
+            Program.UpdateQuality();
+
+            int newSellIn = legendaryItem.SellIn;
+            int newQuality = legendaryItem.Quality;
+
+            Assert.Equal(currentSellIn, newSellIn);
+            Assert.Equal(currentQuality, newQuality);
         }
     }
 }
