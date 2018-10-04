311d641fab06ccd5a172ae1aa4280e5374083415
ApplicationDeveloper <ApplicationDeveloper@ADAM236>
2018-10-04 09:22:14 -0500

    Fixed a bug with where the SellIn value is decremented.  It was being decremented after some dependent logic had been executed.  A test now passes that was failing.  This issue was mentioned in a previous commit.

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -35,6 +35,11 @@ namespace GildedRose.Console
         {
             for (var i = 0; i < Items.Count; i++)
             {
+                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                }
+
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
@@ -70,11 +75,6 @@ namespace GildedRose.Console
                             }
                         }
                     }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
                 if (Items[i].SellIn < 0)
