f411cf5c876b4f1134d72876442e65f436786c2a
ApplicationDeveloper <ApplicationDeveloper@ADAM236>
2019-03-04 09:12:07 -0600

    Flattened if statement nesting for concert ticket logic.  Tests still pass.

--- src/GildedRose.Console/IncreasingQualityWithExpirationItem.cs
+++ src/GildedRose.Console/IncreasingQualityWithExpirationItem.cs
@@ -15,29 +15,33 @@ namespace GildedRose.Console
         }
         public void UpdateItemQuality()
         {
-            if (_dailyCloseItem.Quality < 50)
-            {
-                _dailyCloseItem.Quality = _dailyCloseItem.Quality + 1;
-
-                if (_dailyCloseItem.SellIn < 11)
-                {
-                    if (_dailyCloseItem.Quality < 50)
-                    {
-                        _dailyCloseItem.Quality = _dailyCloseItem.Quality + 1;
-                    }
-                }
-
-                if (_dailyCloseItem.SellIn < 6)
-                {
-                    if (_dailyCloseItem.Quality < 50)
-                    {
-                        _dailyCloseItem.Quality = _dailyCloseItem.Quality + 1;
-                    }
-                }
-            }
             if (_dailyCloseItem.SellIn < 0)
             {
                 _dailyCloseItem.Quality = _dailyCloseItem.Quality - _dailyCloseItem.Quality;
+                return;
+            }
+
+            if (_dailyCloseItem.Quality >= 50)
+            {
+                return;
+            }
+
+            if (_dailyCloseItem.SellIn < 6)
+            {
+                _dailyCloseItem.Quality = _dailyCloseItem.Quality + 3;
+            }
+            else if (_dailyCloseItem.SellIn < 11)
+            {
+                _dailyCloseItem.Quality = _dailyCloseItem.Quality + 2;
+            }
+            else
+            {
+                _dailyCloseItem.Quality = _dailyCloseItem.Quality + 1;
+            }
+
+            if (_dailyCloseItem.Quality > 50)
+            {
+                _dailyCloseItem.Quality = 50;
             }
         }
     }
